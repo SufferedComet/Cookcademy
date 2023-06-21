1254024232e99b46a7b5516e12af09333f90ad17
TestName <48367659+SufferedComet@users.noreply.github.com>
2023-06-21 10:51:53 -0700

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Angelo Delgado on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
